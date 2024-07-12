9e12a47e8845eb166729801389fdd56eca661194
M-Roesner <markus_roesner@web.de>
2024-07-12 18:31:57 +0200

    Updated user component with 'signal' statemanagement.

--- 02-essentials/src/app/user/user.component.ts
+++ 02-essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,14 +12,33 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  /** without signal */
+  // selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }
+  // get imagePath() {
+  //   return `assets/users/${this.selectedUser.avatar}`;
+  // }
+
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser = DUMMY_USERS[randomIndex];
+  // }
+
+  /**
+   * Statemanagement with signal from '@angular/core':
+   *
+   * Signal is an object that stores a value (any type of value, including nested objects)
+   * When a change occurs, Angular is then able to update the part of the UI that needs updating.
+   *
+   * Hint: This is provided since Angular 16 / 17
+   */
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
--- courses.md
+++ courses.md
@@ -5,3 +5,6 @@ Note: If you are starting with a new copied project! Go to the project directory
 # Links:
 
 [02-essentials](https://github.com/mschwarzmueller/angular-complete-guide-course-resources/blob/main/attachments/02-essentials/01-starting-project.zip)
+
+- Signal is used in this course as a state management system! Currently only in the user component.
+- Note: Signal is available since Angular version 16/17.
