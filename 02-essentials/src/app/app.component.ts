f859c4bb55c78fcb1f52cd124bb25d1a2f5360bf
M-Roesner <markus_roesner@web.de>
2024-07-12 20:05:14 +0200

    Added task component from user.

--- 02-essentials/src/app/app.component.ts
+++ 02-essentials/src/app/app.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from './dummy-users';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-
-import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
 
+  // TASK from the course: Create a task component and give it the name that is also to be displayed.
+
+  selectedUserId = 'u1';
+
+  //
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectUser(id: string) {
-    console.log(`Selected user ${id}`);
+    this.selectedUserId = id;
   }
 }
