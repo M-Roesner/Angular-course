1a82a171bcaa7ddf175a1a247dccde87cb3a1d08
M-Roesner <markus_roesner@web.de>
2024-07-12 22:47:16 +0200

    Added new todo component and opening handling.

--- 02-essentials/src/app/tasks/tasks.component.ts
+++ 02-essentials/src/app/tasks/tasks.component.ts
@@ -2,19 +2,22 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() userId?: string;
+  @Input() userId!: string;
   @Input() name?: string;
 
   filteredTasks = DUMMY_TASKS;
+
+  isAddingTask = false;
 
   get selectedUserTasks() {
     return this.filteredTasks.filter((task) => task.userId === this.userId);
@@ -22,5 +25,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.filteredTasks = this.filteredTasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
