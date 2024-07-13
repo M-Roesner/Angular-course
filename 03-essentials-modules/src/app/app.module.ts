2047aca046048427104367cb7d7be703f7fc6bd4
M-Roesner <markus_roesner@web.de>
2024-07-13 13:05:41 +0200

    Changed HeaderComponent to a module.

--- 03-essentials-modules/src/app/app.module.ts
+++ 03-essentials-modules/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   bootstrap: [AppComponent],
   // imports: [] - This is for the usage with standalone components,
   // but this need the BrowserModule from '@angular/platform-browser'.
   // With this you can mix angual components and mudules!
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  imports: [BrowserModule, UserComponent, TasksComponent],
 })
 export class AppModule {}
