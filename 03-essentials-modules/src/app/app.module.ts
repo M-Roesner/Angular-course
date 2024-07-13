01100ffc86fd58db3ce308447ae7cc45808a299b
M-Roesner <markus_roesner@web.de>
2024-07-13 13:29:20 +0200

    Migration TasksComponent and NewTaskComponent from component to module.

--- 03-essentials-modules/src/app/app.module.ts
+++ 03-essentials-modules/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
   bootstrap: [AppComponent],
   // imports: [] - This is for the usage with standalone components,
   // but this need the BrowserModule from '@angular/platform-browser'.
   // With this you can mix angual components and mudules!
-  imports: [BrowserModule, UserComponent, TasksComponent],
+  imports: [
+    BrowserModule,
+    // DatePipe, // DatePipe is not needed, because it's already available in 'BrowserModule'.
+    FormsModule,
+  ],
 })
 export class AppModule {}
