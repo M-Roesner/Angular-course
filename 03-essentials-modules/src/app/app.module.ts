cfe08084c6e61f9a45c33ea317b22ea83b20c123
M-Roesner <markus_roesner@web.de>
2024-07-13 13:59:50 +0200

    Changed TasksComponent's to an own Module.

--- 03-essentials-modules/src/app/app.module.ts
+++ 03-essentials-modules/src/app/app.module.ts
@@ -6,31 +6,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
 
 // Modules
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks-modules';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
   // imports: [] - This is for the usage with standalone components,
   // but this need the BrowserModule from '@angular/platform-browser'.
   // With this you can mix angual components and mudules!
   imports: [
-    BrowserModule,
-    // DatePipe, // DatePipe is not needed, because it's already available in 'BrowserModule'.
-    FormsModule,
+    BrowserModule, // BrowserModule - is only used in the Root Module.
     SharedModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
