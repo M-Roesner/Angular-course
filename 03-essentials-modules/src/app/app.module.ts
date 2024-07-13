4f7e41d00bde1874d56062111c00b8c747f740e1
M-Roesner <markus_roesner@web.de>
2024-07-13 13:36:23 +0200

    Added own shared.module for handling the the card style component.

--- 03-essentials-modules/src/app/app.module.ts
+++ 03-essentials-modules/src/app/app.module.ts
@@ -7,16 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TaskComponent } from './tasks/task/task.component';
+
+// Modules
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
@@ -29,6 +30,7 @@ import { TaskComponent } from './tasks/task/task.component';
     BrowserModule,
     // DatePipe, // DatePipe is not needed, because it's already available in 'BrowserModule'.
     FormsModule,
+    SharedModule,
   ],
 })
 export class AppModule {}
