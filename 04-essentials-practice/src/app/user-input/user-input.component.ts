2518a67affd3088bd39e7a379688a314f2a2a908
M-Roesner <markus_roesner@web.de>
2024-08-19 16:12:09 +0200

    Pass the calculation function to the user input component and receive the data back.

--- 04-essentials-practice/src/app/user-input/user-input.component.ts
+++ 04-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,16 +9,33 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    console.log('SUBMITTED!');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.calculate.emit({
+      /**
+       * Transforming the value to a number use ...
+       *
+       * Number()
+       * - or -
+       * + in front of the attribute name
+       *
+       * Hint: '+' is a shortcut for transforming
+       */
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: Number(this.enteredExpectedReturn),
+      annualInvestment: Number(this.enteredAnnualInvestment),
+    });
   }
 }
