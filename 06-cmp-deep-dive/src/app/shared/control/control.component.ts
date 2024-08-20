64a0cb54e0b6308a8a34fc7e273b80eda3ce8b5d
M-Roesner <markus_roesner@web.de>
2024-08-20 18:44:54 +0200

    Added click event and received reference from the host component.

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,11 +14,23 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None, // Read the comments in the html file!
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log(`Clicked on ${this.label}`);
+  // }
+  private el = inject(ElementRef);
+
   label = input.required<string>();
+
+  onClick() {
+    console.log(`Clicked on ${this.label}`);
+    console.log(this.el);
+  }
 }
