bee8b1f22063eab64d14c01df7ebb55b585e41ab
M-Roesner <markus_roesner@web.de>
2024-08-21 17:41:49 +0200

    Added custom two way binding, how does it work with decorator or signal.

--- custom-2w-binding/src/app/rect/rect.component.ts
+++ custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  input,
+  model,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -10,7 +17,27 @@ import { Component } from '@angular/core';
 export class RectComponent {
   // Todo: Implement custom two-way binding
 
+  // Two Way binding with the decorator:
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // // Attention: The name for two way binding is important the name has to end with "Change"
+  // // Error message:
+  // // "The property and event halves of the two-way binding 'size' are not bound to the same target."
+
+  // Two Way binding with signal (model):
+  size = model.required<{ width: string; height: string }>();
+
   onReset() {
-    // ...
+    // With the decorator:
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100',
+    // });
+
+    // With signal (model):
+    this.size.set({
+      width: '200',
+      height: '100',
+    });
   }
 }
