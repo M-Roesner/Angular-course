8d1b403953930d468d1482b3cc27a98dbfc13e97
M-Roesner <markus_roesner@web.de>
2024-08-21 13:51:20 +0200

    Added OnDestroy for the destroying of an interval.

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,27 +7,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
   // 'implements OnInit', is recommended to prevent bugs for incorrect write-whitening
   // if you want to use ngOnInit instead of the constructor.
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
-    // setInterval(() => {
-    //   const rnd = Math.random();
-    //   if (rnd < 0.5) this.currentStatus = 'online';
-    //   else if (rnd < 0.9) this.currentStatus = 'offline';
-    //   else this.currentStatus = 'unknown';
-    // }, 5000);
-  }
+  // ReturnType<typeof setInterval> - gets the correct return type of setInterval
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+
+    this.interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) this.currentStatus = 'online';
       else if (rnd < 0.9) this.currentStatus = 'offline';
       else this.currentStatus = 'unknown';
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
+  ngOnDestroy(): void {
+    // cleans the interval for preventing memory leaks
+    clearTimeout(this.interval);
   }
 }
