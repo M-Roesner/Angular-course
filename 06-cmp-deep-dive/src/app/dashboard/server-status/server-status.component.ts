02cc36fa90895c3eef83fc49bf75407a61b57a90
M-Roesner <markus_roesner@web.de>
2024-08-21 13:20:56 +0200

    Added ngOnInit as alternitve to constructor.

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
+  // 'implements OnInit', is recommended to prevent bugs for incorrect write-whitening
+  // if you want to use ngOnInit instead of the constructor.
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
   constructor() {
+    // setInterval(() => {
+    //   const rnd = Math.random();
+    //   if (rnd < 0.5) this.currentStatus = 'online';
+    //   else if (rnd < 0.9) this.currentStatus = 'offline';
+    //   else this.currentStatus = 'unknown';
+    // }, 5000);
+  }
+
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
 
