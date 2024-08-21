fb69d2a4f9046d06e5c13eb0598e53af36ff7284
M-Roesner <markus_roesner@web.de>
2024-08-21 16:00:53 +0200

    Added init hooks to show how it works.

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,14 +17,24 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
-  private form = viewChild<ElementRef<HTMLFormElement>>('form'); // with signal since Angualr v17.3
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); // with signal since Angualr v17.3
+
+  ngOnInit() {
+    console.log('OnInit');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('After View Init');
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(title: string, ticketText: string) {
     console.log('title:' + title);
     console.log('ticketText:' + ticketText);
-    this.form()?.nativeElement.reset(); // Reset the form after submission
+    this.form?.nativeElement.reset(); // Reset the form after submission
 
     // Alternatively, you can pass the 'form' template variable as an angument
     // to the onSubmit() like the title and then reset it:
