4ff174cf6d961190becd781e2a04874d6883e999
M-Roesner <markus_roesner@web.de>
2024-08-21 15:05:57 +0200

    Added reset of the form field.

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild<ElementRef<HTMLFormElement>>('form'); // with signal since Angualr v17.3
+
   onSubmit(title: string, ticketText: string) {
     console.log('title:' + title);
     console.log('ticketText:' + ticketText);
+    this.form()?.nativeElement.reset(); // Reset the form after submission
+
+    // Alternatively, you can pass the 'form' template variable as an angument
+    // to the onSubmit() like the title and then reset it:
+    // form.reset();
   }
 }
