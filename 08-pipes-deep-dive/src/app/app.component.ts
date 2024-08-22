eff1132869e0762c0782692386c56dcf8006310a
M-Roesner <markus_roesner@web.de>
2024-08-22 12:45:17 +0200

    Fix temperature update by creating a new array instance due to pipe immutability

--- 08-pipes-deep-dive/src/app/app.component.ts
+++ 08-pipes-deep-dive/src/app/app.component.ts
@@ -22,7 +22,19 @@ export class AppComponent {
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
+  constructor() {
+    // this.historicTemperatures.sort((a, b) => b - a); // If you sort here you can remove the sort pipe from the template!
+  }
+
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    // this.historicTemperatures[index] = 18;
+
+    // We need to create a new instance of the temperatures array because the sort pipe (| sort: 'desc') returns a new sorted array
+    // The original array reference is not updated by the pipe, so we must manually create a new array to update the temperatures correctly.
+    const newTemps = (this.historicTemperatures = [
+      ...this.historicTemperatures,
+    ]);
+    newTemps[index] = 18;
+    this.historicTemperatures = newTemps;
   }
 }
--- 08-pipes-deep-dive/src/app/sort.pipe.ts
+++ 08-pipes-deep-dive/src/app/sort.pipe.ts
@@ -3,6 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sort',
   standalone: true,
+  pure: false, // This forces the pipe to re-render, makes the pipe immutable by default and avoids unnecessary re-rendering.
+  // But this is not recommended because the pipe can / will be re-rendered often.
 })
 export class SortPipe implements PipeTransform {
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
