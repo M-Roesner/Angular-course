566e3fa76b69d86dd82d51adf5872dc2f8e4d676
M-Roesner <markus_roesner@web.de>
2024-08-22 09:06:40 +0200

    Added a custom attribute directive to change an anchor element.

--- 07-directives-deep-dive/src/app/safe-link.directive.ts
+++ 07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -3,9 +3,24 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+    '[target]': '_blank',
+    '[rel]': 'noopener',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    // If an anchor element (a-tag) is clicked it will be a 'MouseEvent' automatically
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+    if (wantsToLeave) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
