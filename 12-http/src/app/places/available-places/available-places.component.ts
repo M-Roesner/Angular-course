25bc9ce80feb02eb227a3d5eb2a4973d39b75063
M-Roesner <markus_roesner@web.de>
2024-08-23 17:32:28 +0200

    Stores the response.

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -23,9 +24,13 @@ export class AvailablePlacesComponent implements OnInit {
   ngOnInit() {
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places)) // Pipe is not necessarily needed.
       .subscribe({
-        next: (resData) => {
-          console.log(resData);
+        // next: (resData) => { // Alternatively, you can use the pipe function before.
+        //   this.places.set(resData.places);
+        // },
+        next: (places) => {
+          this.places.set(places);
         },
       });
 
