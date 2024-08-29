08f76a649ef7070cef4d29c0bc4451f27536b337
M-Roesner <markus_roesner@web.de>
2024-08-29 16:43:53 +0200

    Added saving and reading of input data in local storage.

--- 13-forms/src/app/auth/login-via-reactive/login-via-reactive.component.ts
+++ 13-forms/src/app/auth/login-via-reactive/login-via-reactive.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -18,10 +18,21 @@ function mustContainQuestionMark(control: AbstractControl) {
 
 function emailIsUnique_dummyValidator(control: AbstractControl) {
   if (control.value !== 'test@example.com') {
+    // of() is an observable of RxJS
     return of(null);
   }
 
   return of({ notUnique: true });
+}
+
+// Initial value for the email field (if any) loaded from local storage (outside of the component):
+// This will be executed when the file will be executed for the first time in the browser.
+// This won't work for server-side rendering, but only for client-side rendering.
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem('saved-login-form');
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
 }
 
 @Component({
@@ -31,11 +42,14 @@ function emailIsUnique_dummyValidator(control: AbstractControl) {
   templateUrl: './login-via-reactive.component.html',
   styleUrl: './login-via-reactive.component.css',
 })
-export class LoginViaReactiveComponent {
+export class LoginViaReactiveComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   // Set up the form (FormGroup and FormControl are classes and must be initialized first):
   myform = new FormGroup({
     // lets angualar know, how is this form connected to the actual templete (input elements)
-    email: new FormControl('', {
+    // email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.email, Validators.required],
       asyncValidators: [emailIsUnique_dummyValidator],
     }),
@@ -61,6 +75,31 @@ export class LoginViaReactiveComponent {
       this.myform.controls.password.dirty &&
       this.myform.controls.password.invalid
     );
+  }
+
+  ngOnInit(): void {
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   // this.myform.controls.email.setValue(loadedForm.email);
+    //   // patchValue() set the values which are given and let the other values untouched
+    //   this.myform.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+
+    const subscription = this.myform.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 
   onSubmit() {
