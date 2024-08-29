767775f8dead9235218657290d8f99933b4f59d2
M-Roesner <markus_roesner@web.de>
2024-08-29 15:26:01 +0200

    Added an alternative login component via reative.

--- 13-forms/src/app/app.component.ts
+++ 13-forms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { LoginComponentViaTemplateDriven } from './auth/login-via-template-driven/login.component';
+import { LoginViaReactiveComponent } from './auth/login-via-reactive/login-via-reactive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponentViaTemplateDriven],
+  imports: [LoginComponentViaTemplateDriven, LoginViaReactiveComponent],
 })
 export class AppComponent {}
---
+++ 13-forms/src/app/auth/login-via-reactive/login-via-reactive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-via-reactive',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-via-reactive.component.html',
+  styleUrl: './login-via-reactive.component.css',
+})
+export class LoginViaReactiveComponent {}
