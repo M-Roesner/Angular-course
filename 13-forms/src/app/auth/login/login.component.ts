67d8303068d4eab08da3af156216a0e3a102789d
M-Roesner <markus_roesner@web.de>
2024-08-29 14:55:31 +0200

    Reset the form and store the input in the local memory.

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +16,32 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subbscription = this.form()
+        .valueChanges?.pipe(
+          // debounceTime - is an optimization for performance otherwise it will store the value after every change.
+          debounceTime(500)
+        )
+        .subscribe({
+          next: (value) =>
+            // Store the form data in local storage.
+            window.localStorage.setItem(
+              // key in the local storage
+              'safed-login-form',
+              // value to be stored in the local storage
+              // The value must be changed to a json string before.
+              JSON.stringify({ email: value.email })
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => subbscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) return;
 
@@ -16,5 +49,7 @@ export class LoginComponent {
     const enteredPassword = formData.form.value.password;
     console.log(formData.form);
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
   }
 }
