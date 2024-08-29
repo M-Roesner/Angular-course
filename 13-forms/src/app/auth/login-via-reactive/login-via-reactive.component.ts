d4b1489626e5a3c9d24b24885833bde465948c0c
M-Roesner <markus_roesner@web.de>
2024-08-29 15:45:18 +0200

    Added connection of the form. Between the html and ts file.

--- 13-forms/src/app/auth/login-via-reactive/login-via-reactive.component.ts
+++ 13-forms/src/app/auth/login-via-reactive/login-via-reactive.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-via-reactive',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-via-reactive.component.html',
   styleUrl: './login-via-reactive.component.css',
 })
-export class LoginViaReactiveComponent {}
+export class LoginViaReactiveComponent {
+  // Set up the form (FormGroup and FormControl are classes and must be initialized first):
+  myform = new FormGroup({
+    // lets angualar know, how is this form connected to the actual templete (input elements)
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {}
+}
