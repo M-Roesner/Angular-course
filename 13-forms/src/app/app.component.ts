4b650ce1035190838aa138765800eb921e13a40b
M-Roesner <markus_roesner@web.de>
2024-08-29 17:30:56 +0200

    Connected input fields to ts formControles

--- 13-forms/src/app/app.component.ts
+++ 13-forms/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 import { LoginComponentViaTemplateDriven } from './auth/login-via-template-driven/login.component';
 import { LoginViaReactiveComponent } from './auth/login-via-reactive/login-via-reactive.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponentViaTemplateDriven, LoginViaReactiveComponent],
+  imports: [
+    LoginComponentViaTemplateDriven,
+    LoginViaReactiveComponent,
+    SignupComponent,
+  ],
 })
 export class AppComponent {}
--- 13-forms/src/app/auth/signup/signup.component.ts
+++ 13-forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,70 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// function checkPasswordIsEqual(controle: AbstractControl){
+
+//   if
+// }
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  myForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', {
+      validators: [Validators.required],
+    }),
+    agree: new FormControl(false, {
+      validators: [Validators.required],
+    }),
+  });
+
+  OnSubmit() {
+    console.log(this.myForm);
+  }
+
+  OnReset() {
+    console.log('onReset');
+
+    this.myForm.reset();
+  }
+}
