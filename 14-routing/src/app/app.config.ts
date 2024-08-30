86999caf9d09c529492ae725507d64a488552cfc
M-Roesner <markus_roesner@web.de>
2024-08-30 12:41:45 +0200

    Get user the user id via component input binding from the url.

--- 14-routing/src/app/app.config.ts
+++ 14-routing/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      // withComponentInputBinding() allows route parameters, query parameters,
+      // and resolved data to be automatically bound to component inputs.
+      withComponentInputBinding()
+    ),
+  ],
 };
