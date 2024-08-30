2d3ec11cd101642c4ae24ce43ec902ee8f079310
M-Roesner <markus_roesner@web.de>
2024-08-30 17:53:52 +0200

    Extracting Query Parameters via Observables

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,8 +1,15 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -11,15 +18,29 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
-
-  // Attention the name (in this case 'order') has to be the same name as in the template!
-  order = input<'asc' | 'desc'>();
 
   private tasksService = inject(TasksService);
 
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
   );
+
+  // Extracting Query Parameters via Inputs
+  // Attention the name (in this case 'order') has to be the same name as in the template!
+  // order = input<'asc' | 'desc'>();
+
+  // Extracting Query Parameters via Observables
+  order?: 'asc' | 'desc';
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
