a602e9b6251d81dab98cf2e54c4f21ebe2f171b2
M-Roesner <markus_roesner@web.de>
2024-08-30 14:38:16 +0200

    Add fallback page if no url found

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -23,5 +24,10 @@ export const routes: Routes = [
         component: NewTaskComponent,
       },
     ],
+  },
+  {
+    // Fallback routes if no url found
+    path: '**', // <your-domain>/*
+    component: NotFoundComponent,
   },
 ];
