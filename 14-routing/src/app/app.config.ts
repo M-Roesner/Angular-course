2e8ec90f91fcd510735d9b3ccc65c1baf60153fc
M-Roesner <markus_roesner@web.de>
2024-08-30 14:07:50 +0200

    Get access to user id (parameter) from the parent element.

--- 14-routing/src/app/app.config.ts
+++ 14-routing/src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -9,7 +13,13 @@ export const appConfig: ApplicationConfig = {
       routes,
       // withComponentInputBinding() allows route parameters, query parameters,
       // and resolved data to be automatically bound to component inputs.
-      withComponentInputBinding()
+      withComponentInputBinding(),
+      // withRouterConfig() allows configuring the router's behavior.
+      // Setting paramsInheritanceStrategy to 'always' ensures that
+      // route parameters from parent routes are always available in child routes.
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
     ),
   ],
 };
